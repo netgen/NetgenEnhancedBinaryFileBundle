342c7947e1cde143cdd25217a168f01882a6539f
Hrvoje Knežević <hrvoje.k@netgen.hr>
2015-10-19 17:10:10 +0200

    Add dependency injection configuration

---
+++ DependencyInjection/NetgenEnhancedBinaryFileExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Netgen\Bundle\EnhancedBinaryFileBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\Yaml\Yaml;
+
+class NetgenEnhancedBinaryFileExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load( array $configs, ContainerBuilder $container )
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration( $configuration, $configs );
+
+        $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load( 'fieldtypes.yml' );
+        $loader->load( 'storage_engines.yml' );
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function prepend( ContainerBuilder $container )
+    {
+        $configFile = __DIR__ . '/../Resources/config/ezpublish.yml';
+        $config = Yaml::parse( file_get_contents( $configFile ) );
+        $container->prependExtensionConfig( 'ezpublish', $config );
+        $container->addResource( new FileResource( $configFile ) );
+    }
+}
