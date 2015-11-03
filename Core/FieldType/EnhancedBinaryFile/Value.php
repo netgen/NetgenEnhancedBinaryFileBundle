ca10d1c51b2ad972ea37e2c617010a256580ce49
Filip Božanović <filip@netgen.hr>
2015-11-03 13:00:40 +0100

    Add support for eZ 5 Public API. Field type validates MIME type

---
+++ Core/FieldType/EnhancedBinaryFile/Value.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Netgen\Bundle\EnhancedBinaryFileBundle\Core\FieldType\EnhancedBinaryFile;
+
+use eZ\Publish\Core\FieldType\BinaryFile\Value as BinaryFileValue;
+
+class Value extends BinaryFileValue
+{
+}
