6d5f9015d5b6d4dc585b44eba2b314123973598b
Mario Blažek <blazek.mario@gmail.com>
2017-09-18 15:02:27 +0200

    EnhancedBinaryFileHandler test

---
+++ tests/FieldHandler/EnhancedBinaryFileHandlerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Netgen\Bundle\EnhancedBinaryFileBundle\Tests\FieldHandler;
+
+use eZ\Publish\Core\IO\IOServiceInterface;
+use eZ\Publish\Core\IO\Values\BinaryFileCreateStruct;
+use eZ\Publish\Core\Repository\Values\ContentType\FieldDefinition;
+use Netgen\Bundle\EnhancedBinaryFileBundle\FieldHandler\EnhancedBinaryFileHandler;
+use Netgen\Bundle\InformationCollectionBundle\FieldHandler\Custom\CustomLegacyFieldHandlerInterface;
+use Netgen\Bundle\EnhancedBinaryFileBundle\Core\FieldType\EnhancedBinaryFile\Value;
+use eZ\Publish\Core\FieldType\Integer\Value as IntValue;
+use eZ\Publish\Core\IO\Values\BinaryFile;
+use PHPUnit\Framework\TestCase;
+
+class EnhancedBinaryFileHandlerTest extends TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $io;
+
+    /**
+     * @var EnhancedBinaryFileHandler
+     */
+    protected $handler;
+
+    public function setUp()
+    {
+        $this->io = $this->createMock(IOServiceInterface::class);
+        $this->handler = new EnhancedBinaryFileHandler($this->io);
+    }
+
+    public function testInstanceOfCustomLegacyFieldHandlerInterface()
+    {
+        $this->assertInstanceOf(CustomLegacyFieldHandlerInterface::class, $this->handler);
+    }
+
+    public function testSupports()
+    {
+        $this->assertTrue($this->handler->supports(new Value()));
+        $this->assertFalse($this->handler->supports(new IntValue(1)));
+    }
+
+    public function testToString()
+    {
+        $file = new Value(
+            [
+                'path' => __DIR__ . '/test.txt',
+            ]
+        );
+
+        $this->assertEquals("", $this->handler->toString($file, new FieldDefinition()));
+    }
+
+    public function testGetLegacyValue()
+    {
+        $file = new Value(
+            [
+                'path' => __DIR__ . '/test.txt',
+            ]
+        );
+
+        $struct = new BinaryFileCreateStruct();
+        $binaryFile = new BinaryFile();
+
+        $this->io->expects($this->once())
+            ->method('newBinaryCreateStructFromLocalFile')
+            ->with($file->inputUri)
+            ->willReturn($struct);
+
+        $this->io->expects($this->once())
+            ->method('createBinaryFile')
+            ->with($struct)
+            ->willReturn($binaryFile);
+
+        $data = $this->handler->getLegacyValue($file, new FieldDefinition(['id' => 123]));
+    }
+}
