83950bdd3dd8779177fd019799ad6e0b43bcbeb5
Mario Blažek <blazek.mario@gmail.com>
2017-10-19 13:31:25 +0200

    CS fix

--- bundle/DependencyInjection/Configuration.php
+++ bundle/DependencyInjection/Configuration.php
@@ -15,10 +15,6 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('netgen_enhanced_ez_binary_file');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
